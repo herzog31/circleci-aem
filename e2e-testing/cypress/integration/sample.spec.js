367962b7442b43069a049c5158589611b7cf9369
Mark J. Becker <herzog31@users.noreply.github.com>
2019-11-04 15:00:39 +0100

    Optimize cypress

--- e2e-testing/cypress/integration/sample.spec.js
+++ e2e-testing/cypress/integration/sample.spec.js
@@ -7,8 +7,8 @@ describe('Sample Test', () => {
         }
     };
 
-    it('open Venia homepage', () => {
-        cy.visit('http://localhost:4502/content/venia.html', { ...auth });
+    it('opens Venia homepage', () => {
+        cy.visit('/content/venia.html', { ...auth });
 
         cy.contains('This page redirects to');
         cy.get('a').should('have.attr', 'href', '/content/venia/us/en.html');
